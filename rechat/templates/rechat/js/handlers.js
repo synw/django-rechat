124a39c1419a79ae969ff6d2e6a6957ac3866c9c
syn <synwe@yahoo.fr>
2016-09-01 15:07:40 +0000

    Correction

--- rechat/templates/rechat/js/handlers.js
+++ rechat/templates/rechat/js/handlers.js
@@ -1,10 +1,5 @@
 if (event_class == '__chat_message__') {
-	var hours = timestamp.getHours();
-	if (hours.length==1) {
-		hours = "0"+hours;
-	}
-	var minutes = "0" + timestamp.getMinutes();
-	var msgtime = hours + ':' + minutes.substr(-2);
+	var msgtime = getClockTime(true);
 	$('#chatbox').append('<a name="'+timestamp+'"></a>'+msgtime+' <strong>'+data['username']+'</strong>: '+message+'<br />');
 	$('#chatbox').scrollTop($("#chatbox")[0].scrollHeight);
 	return false
